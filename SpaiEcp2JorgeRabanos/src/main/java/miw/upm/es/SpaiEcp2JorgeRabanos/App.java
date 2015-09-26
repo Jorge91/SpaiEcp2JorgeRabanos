eeec00cf705343066525816d8103b602702b3733
Jorge91 <sirjorgete@gmail.com>
2015-09-26 13:48:50 +0200

    Initial commit. Project creation

---
+++ SpaiEcp2JorgeRabanos/src/main/java/miw/upm/es/SpaiEcp2JorgeRabanos/App.java
@@ -0,0 +1,13 @@
+package miw.upm.es.SpaiEcp2JorgeRabanos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

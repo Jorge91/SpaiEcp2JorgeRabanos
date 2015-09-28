f1e22cc939056bc53a5f6a1db636e916b6afa9ab
Jorgelou <sirjorgete@gmail.com>
2015-09-29 00:14:31 +0200

    DecimalCollectionTest

---
+++ SpaiEcp2JorgeRabanos/src/test/java/miw/upm/es/SpaiEcp2JorgeRabanos/DecimalCollectionTest.java
@@ -0,0 +1,36 @@
+package miw.upm.es.SpaiEcp2JorgeRabanos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DecimalCollectionTest {
+	private DecimalCollection decimalCollection;
+	
+	@Before
+	public void setUp() {
+		this.decimalCollection = new DecimalCollection();
+		
+		this.decimalCollection.add(2.0);
+		this.decimalCollection.add(1.2);
+	}
+
+	@Test
+	public void addTest() {
+		assertEquals(this.decimalCollection.size(), 2);
+	}
+	
+	@Test
+	public void sumTest() {
+		assertEquals(this.decimalCollection.sum(), 3.2, 0);
+	}
+	
+	@Test
+	public void higherTest() {
+		assertEquals(this.decimalCollection.higher(), 2.0, 0);
+	}
+	
+	
+
+}

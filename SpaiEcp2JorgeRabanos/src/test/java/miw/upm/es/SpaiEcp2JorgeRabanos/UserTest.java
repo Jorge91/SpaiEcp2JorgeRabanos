f01f0de9c869fd2ac54931fdc812bb3bbe3ea0ec
Jorgelou <sirjorgete@gmail.com>
2015-09-29 00:13:58 +0200

    UserTest

---
+++ SpaiEcp2JorgeRabanos/src/test/java/miw/upm/es/SpaiEcp2JorgeRabanos/UserTest.java
@@ -0,0 +1,42 @@
+package miw.upm.es.SpaiEcp2JorgeRabanos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+
+private User user;
+	
+	@Before
+	public void setUp() {
+		this.user = new User(1, "Homer", "Simpson");
+	}
+
+	@Test
+	public void fullnameTest() {
+		assertEquals(this.user.fullName(), "Homer Simpson");
+	}
+	
+	@Test
+	public void initialsTest() {
+		assertEquals(this.user.initials(), "H.");
+	}
+	
+	@Test
+	public void numberTest() {
+		assertEquals(this.user.getNumber(), 1, 0);
+	}
+	
+	@Test
+	public void nameTest() {
+		assertEquals(this.user.getName(), "Homer");
+	}
+	
+	@Test
+	public void familyNameTest() {
+		assertEquals(this.user.getFamilyName(), "Simpson");
+	}
+
+}

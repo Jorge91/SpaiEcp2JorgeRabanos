cab3116cc1e19405be39470133e5207921e8b8f8
Fernando Moro <fernando.moro@radmas.com>
2015-10-01 13:29:26 +0200

    Added distancia() method in Point class

--- src/main/java/miw/upm/es/SpaiEcp2JorgeRabanos/Point.java
+++ src/main/java/miw/upm/es/SpaiEcp2JorgeRabanos/Point.java
@@ -30,6 +30,10 @@ public class Point {
         this.y -= origin.getY();
     }
     
+    public double distancia(Point p2) {
+        return Math.sqrt(Math.pow((this.x - p2.x), 2) + Math.pow((this.y - p2.y), 2));
+    }
+    
     public int getX() {
         return this.x;
     }
